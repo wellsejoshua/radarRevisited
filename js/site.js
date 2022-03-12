9699775569f324c8c84cc97031fc2c61ffd561dc
wellsejoshua <84493818+wellsejoshua@users.noreply.github.com>
2022-03-12 00:59:50 -0600

    finished radar again

--- js/site.js
+++ js/site.js
@@ -1,61 +1,49 @@
 //Controller function
+//get values
 function getValues(){
+    // make sure alert is invisible
     document.getElementById("alert").classList.add("invisible");
-    document.getElementById("para").classList.add("invisible");
-    let strInput = document.getElementById("palindromeString").value; 
-    let testString = removeSpecialChrAndSpaces(strInput);
-    let revString = reverseString(strInput);
-    let isPalindrome = compareStrings(strInput,revString);
-    displayString(revString, strInput, isPalindrome);
+    let palindromeString = document.getElementById("palindromeString").value; 
+    
+    //check for palindrome
+    let returnObj = checkForPalindrome(palindromeString);
+
+    //display message out to screen
+    displayMessage(returnObj);
     
 }
 
 
-//logic function
-function reverseString(strInput){
-
-    let revString = [];
-    for (let index = strInput.length - 1; index >= 0; index--) {
-        revString += strInput[index];
+//check if palindrome
+function checkForPalindrome(palindromeString){
+    //convert to lowercasee
+    palindromeString = palindromeString.toLowerCase();
+    //remove spaces and special characters
+    let regex = /[^a-z0-9]/gi;
+    palindromeString = palindromeString.replace(regex,"");
+    let revString =[];
+    let returnObj = {};
+    for (let index = palindromeString.length -1; index >= 0 ; index--) {
+        revString+= palindromeString[index];
     }
-
-    return revString;
-}
-
-
-
-//display/view function
-function displayString(revString, strInput, isPalindrome){
-
-    
-    document.getElementById("h2Heading").innerHTML = "Results";
-    if (isPalindrome == "true") {
-        document.getElementById("para").innerHTML = `Success! ${strInput} is a palindrome. The reversed string is ${revString}.` ;
+    if(revString == palindromeString){
+        returnObj.msg = "Well done! You entered a palindrome";
     }
     else{
-        document.getElementById("para").innerHTML = `${strInput} is Not a palindrome. The reversed string is ${revString}.` ;
+        returnObj.msg = "Sorry! You did not enter a palindrome"
     }
+
+    returnObj.reversed = revString;
+    return returnObj;
+
     
+}
+
+//display message
+function displayMessage(returnObj){
+    document.getElementById("alertHeader").innerHTML = returnObj.msg;
+    document.getElementById("msg").innerHTML = `Your phrase reversed is: ${returnObj.reversed} `;
     document.getElementById("alert").classList.remove("invisible");
-    document.getElementById("para").classList.remove("invisible");
-    
-
 }
 
-//function to remove special characters
-function removeSpecialChrAndSpaces(inputString){
-    let x = typeof(inputString);
-    let testString = "";
-    let specialChrArray = [" "]
-    
-    return testString;
 
-}
-
-function compareStrings(originalString, reversedString){
-    let isPalindrome = "false";
-    if (originalString == reversedString) {
-        isPalindrome = "true";
-    } 
-    return isPalindrome;
-}
